b3c45c9f082654ac382aec9b329138760828772f
Ilya Afanasev <ilya.orsk@gmail.com>
2017-06-13 01:41:40 +0300

    Add ebo for rendering square

--- create_triangle/main.cpp
+++ create_triangle/main.cpp
@@ -8,9 +8,15 @@
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
+        0.5f,  0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+};
+
+GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
 };
 
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -112,20 +118,22 @@ int main()
 
     GLuint vbo = 0;
     GLuint vao = 0;
+    GLuint ebo = 0;
 
     glGenBuffers(1, &vbo);
     glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &ebo);
 
     glBindVertexArray(vao);
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*) 0);
     glEnableVertexAttribArray(0);
-
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     // Game loop
     while (!glfwWindowShouldClose(window))
     {
@@ -134,17 +142,17 @@ int main()
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        // Render
 
+        // Render
         glUseProgram(shader_program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
-
         glfwSwapBuffers(window);
     }
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
