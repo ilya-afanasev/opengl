e9142d8098952522f1a180b29ce8f72d78b99187
Ilya Afanasev <ilya.orsk@gmail.com>
2017-06-13 23:42:29 +0300

    Add using second attribute for vertex shader

--- create_triangle/main.cpp
+++ create_triangle/main.cpp
@@ -9,15 +9,12 @@
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 GLfloat vertices[] = {
-        0.5f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 };
-
 GLuint indices[] = {
-        0, 1, 3,
-        1, 2, 3
+        0, 1, 2,
 };
 
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -25,15 +22,18 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 const char* const vertex_shader = "#version 330 core\n"
         "\n"
         "layout (location = 0) in vec3 position;\n"
-        "\n\n"
+        "layout (location = 1) in vec4 color;\n"
+        "out vec4 vertex_color;\n"
+        "\n"
         "void main()\n"
         "{\n"
         "    gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+        "    vertex_color = color;"
         "}";
 
 const char* const fragment_shader = "#version 330 core\n"
         "\n"
-        "uniform vec4 vertex_color;\n"
+        "in vec4 vertex_color;\n"
         "out vec4 color;\n"
         "\n"
         "void main()\n"
@@ -126,12 +126,19 @@ int main()
     glGenBuffers(1, &ebo);
 
     glBindVertexArray(vao);
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*) 0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 //    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -144,11 +151,11 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        GLdouble current_time = glfwGetTime();
-        GLfloat green = static_cast<GLfloat>((sin(current_time) / 2) + 0.5);
-        GLint vertex_color = glGetUniformLocation(shader_program, "vertex_color");
+//        GLdouble current_time = glfwGetTime();
+//        GLfloat green = static_cast<GLfloat>((sin(current_time) / 2) + 0.5);
+//        GLint vertex_color = glGetUniformLocation(shader_program, "vertex_color");
         glUseProgram(shader_program);
-        glUniform4f(vertex_color, 0.0f, green, 0.0f, 1.0f);
+//        glUniform4f(vertex_color, 0.0f, green, 0.0f, 1.0f);
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
